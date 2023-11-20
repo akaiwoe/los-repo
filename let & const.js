f81e15b26438606d46e571bd71a0c0b32f1be3af
akaiwoe <124941095+akaiwoe@users.noreply.github.com>
2023-11-20 18:47:31 -0500

    added an excercise

---
+++ let & const.js
@@ -0,0 +1,13 @@
+const pi = 3.14;
+
+"var can be reassigned, redeclared, and can mutate, however it's scope rule is a function scope";
+
+"let on the otherhand is the same in terms of flexibility however it cannot be redeclared and the scope rules is that it works on block scope";
+
+'const cannot be redeclared or reassigned like var and var can access a hoisted variable, const creates a block scope.';
+
+'You can reassign with let, but you can not redeclare or reassign using the const keyword.';
+
+'Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution. Inevitably, this means that no matter where functions and variables are declared, they are moved to the top of their scope'
+
+
