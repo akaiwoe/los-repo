292aa26c29e3c4032c55ba291bc6a73d585aed25
akaiwoe <124941095+akaiwoe@users.noreply.github.com>
2024-01-25 19:23:16 -0500

    checking functionality of submit button

--- Hack or Snooze Assesment/js/nav.js
+++ Hack or Snooze Assesment/js/nav.js
@@ -34,3 +34,23 @@ function updateNavOnLogin() {
   $navLogOut.show();
   $navUserProfile.text(`${currentUser.username}`).show();
 }
+
+/** Show story creation page on click of submit */
+function navCreateStory(evt) {
+  console.debug("navCreateStory", evt)
+  hidePageComponents();
+  $storyForm.show();
+}
+
+let test = document.querySelector("#story-form")
+test.addEventListener("onclick", function(){
+  if(test.style.display === "none") {
+    test.style.display = "block";
+  } else {
+    test.style.display = "none";
+  }
+});
+
+$("#story-form").click(function(){
+  alert("CLICKED?")
+})
